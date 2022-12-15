bae03b0883908c7670c259c0a35062a485254a74
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-15 23:17:37 +0530

    if_else_statement is added

---
+++ js/if_Else_Statement.js
@@ -0,0 +1,41 @@
+//if else statement --> here we can have 2 statements and conditions
+//  if first condition is true then first statement is exceuted otherwise second statement is executed
+
+var car1="volvo";
+var car2="Benz";
+var car3="BMW";
+
+if (car1="volvo")
+{
+    document.write("yes car1 is volvo");
+}
+else
+{
+    document.write("nope car1 is not volvo");
+}
+
+// example_2:
+document.write("<br>")
+var fname="Roshan";
+var gender="male";
+if(gender="male")
+{
+    document.write("hello Mr."+fname);
+}
+else
+{
+    document.write("hello mrs."+fname)
+}
+
+// example_3:
+document.write("<br>")
+var a=100;
+var b=200;
+
+if(a<b){
+    document.write("a is less than b")
+}
+else
+{
+    document.write("no a is not less than b")
+}
