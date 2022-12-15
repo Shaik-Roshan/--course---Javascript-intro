1fa41ab172209040e5a95ce1b9db66ba826d6037
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-16 00:31:05 +0530

    ternary operator is added

---
+++ js/Ternary_oper.js
@@ -0,0 +1,6 @@
+
+// Ternary operator-->syntax= (condition)? true statement:false statement
+var mar1=90;
+var msg;
+(mar1>50)?msg="hey you have acheived it":msg="better luck next time";
+document.write(msg)
