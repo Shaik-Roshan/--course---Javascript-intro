6b9709ca2e04beedbfd92509fee71a9b8582274a
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-16 20:43:37 +0530

    alert box added

---
+++ js/alert_box.js
@@ -0,0 +1,15 @@
+alert("hello folks we will check how alert box works");
+
+//lets do some example of alert box by using if else statement
+
+var a=20;
+var b=40;
+
+if (a>b)
+{
+    alert("'a' is greater than 'b'"+ "value:"+a);
+}
+else
+{
+    alert("'b is greater" + " hence value:"+b)
+}
