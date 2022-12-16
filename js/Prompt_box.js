17bfc2ef6852a97a9af16399327701516b57b791
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-16 23:23:54 +0530

    prompt_box added

---
+++ js/Prompt_box.js
@@ -0,0 +1,16 @@
+// Example_1
+var a=prompt("enter you name");
+confirm(a)
+if(a)
+{
+    alert("thanks for providing  your details")
+}
+else
+{
+    alert("hey oops you dint submit your details")
+}
+
+// Example_2:
+var student_name=prompt("enter you name")
+var clg_name=prompt("enter your college name");
+document.write(" hello "+student_name + " you have choosen  " +clg_name)
