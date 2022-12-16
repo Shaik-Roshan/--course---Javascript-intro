a68c3a6c16367e8c58fde651685b9b3a4c4a86ea
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-16 22:58:23 +0530

    confirm_box added

---
+++ js/confirm_box.js
@@ -0,0 +1,10 @@
+confirm("yes great"); //this confirm box have two buttons(ok and cancel) with message 
+
+var a=confirm("you have to invest time to learn something");
+if(a)
+{
+    alert("thats great you can do it")//when we click on ok this message will generate
+}
+else{
+    alert("oops atleast you can try to acheive it")//when we click on cancel this message will generate
+}
