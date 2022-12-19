68cadd2b6c80a8d26d357f49403d8627c847afff
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-19 23:27:21 +0530

    evnets added

---
+++ js/events.js
@@ -0,0 +1,16 @@
+// basic events in javascript
+// 1.Click          -->  onclick
+// 2.Double Click   -->  ondbclick
+// 3.Right Click    -->  onContextmenu
+// 4.Mouse hover    -->  onmouseenter
+// 5.Mouse out      -->  onmouseout
+// 6.Mouse Down     -->  onmousedown
+// 7.Mouse Up       -->  onmouseup
+// 8.Key Press      -->  onkeypress
+// 9.Key Up         -->  onkeyup
+// 10.Load          -->  onload
+// 11.Unload        -->  onunload
+// 12.Resize        -->  onresize
+// 13.scroll        -->  onscroll
+
+
