39833b93facafc97a707f59c756a1300e7c47b83
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-19 23:44:27 +0530

    added

--- js/while_loop.js
+++ js/while_loop.js
@@ -3,8 +3,8 @@
 //     statement:
 // }
 
-// var number1=200;
-// while(number1>100){
-//     document.write("yes number1 is greater than 100")
-//     number1 =number1+1;
-// }
+var number1=200;
+while(number1>100){
+    document.write("yes number1 is greater than 100")
+    number1 =number1+1;
+}
