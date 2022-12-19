e61c44fe0f76e13790eb942bbc11f781ebf8a625
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-19 23:39:00 +0530

    while loop added

---
+++ js/while_loop.js
@@ -0,0 +1,10 @@
+// // while loop syntax
+// while(condition){
+//     statement:
+// }
+
+// var number1=200;
+// while(number1>100){
+//     document.write("yes number1 is greater than 100")
+//     number1 =number1+1;
+// }
