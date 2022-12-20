4dbde5eba6a802648a4b4add83a3fdf0c38f6f43
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-20 23:34:42 +0530

    for_loop

---
+++ js/for_loop.js
@@ -0,0 +1,8 @@
+// for loop
+// syntax
+// for(initlization;Condition,inc/dec){
+//     statement
+// }
+for (let a = 0; a <= 10; a++) {
+  document.write(a + " hello world this is for loop" + "<br>");
+}
