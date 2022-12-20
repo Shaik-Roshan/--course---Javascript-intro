d4d4f5098ae7fdc62fbcb6d19fdf2e7d2ea2ae77
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-20 23:25:06 +0530

    Do_while_loop

---
+++ js/Do_while_loop.js
@@ -0,0 +1,12 @@
+// Do While loop
+// syntax
+// Do{
+//     statement
+//     increment/decrement
+// }while(condition)
+
+var a=1;
+do{
+    document.write(a+". "+"hello Roshan_shaik"+"<br>");//here this statement is printed for 10 times till the condition become true and once the condition become false it will come out of the loop
+    a=a+1;
+}while(a<=10)
