d0793b27073f170ba7aaf888ca2439c175af9a22
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-23 21:45:36 +0530

    Nested_loop_2

--- js/Nested_loop_2.js
+++ js/Nested_loop_2.js
@@ -1,6 +1,19 @@
 for(var a = 5; a >= 1;  a-- ){
     for(var b = a; b>=1;b--){
-      document.write(b+" ")
+      document.write(b+" ");
     }
-    document.write("<br>")
+    document.write("<br>");
   }
+//   output
+// 5 4 3 2 1
+// 4 3 2 1
+// 3 2 1
+// 2 1
+// 1
+
+for(c=5;c>=1;c--){
+    for(d=1;d<=c;c++){
+        document.write(c);
+    }
+    document.write("<br>");
+}
