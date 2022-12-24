62061cd514518d172430b6188392b399dc2615a6
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-24 23:47:32 +0530

    Array_push_pop

---
+++ js/Array_push_pop.js
@@ -0,0 +1,8 @@
+var actors=["sharuk khan","Salman khan","Amir khan", "Akshay kumar"];
+document.write(actors+"<br>");
+actors.push("hrithik roshan")
+document.write(actors+"<br>");
+
+actors.pop();
+document.write(actors+"<br>");
+
