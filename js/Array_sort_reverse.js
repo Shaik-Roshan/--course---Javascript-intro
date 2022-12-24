f66d8e656d7a2b9d80c836ed936a884c6a55df5f
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-24 23:30:50 +0530

    Array_sort_reverse

---
+++ js/Array_sort_reverse.js
@@ -0,0 +1,10 @@
+var car=["BMW","Volvo","Skoda","Audi"];
+document.write(car+"<br>");//before modification
+
+//Array sort method()
+car.sort();
+document.write("sorting: "+car+"<br>");//after sorting
+
+//Array reverse method()
+car.reverse();
+document.write("Reverse: "+car);//after reverse
