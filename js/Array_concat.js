ab44b8cdbf0f3a5dde54fb8ed02c91aee3e497fd
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-25 23:42:05 +0530

    Array_concat

---
+++ js/Array_concat.js
@@ -0,0 +1,13 @@
+var a=[1,2,3,4,5];
+var b=[6,7,8,9,10];
+var a_b=[a+b];
+document.write("value of a: "+a+"<br>");
+document.write("value of b: "+b+"<br>");
+document.write("value of both a and b: "+a_b+"<br>");
+
+var subj1=["telugu","hindi","english"];
+var subj2=["Mathematics","physics"];
+document.write(subj1.concat(subj2)+"<br>")
+document.write(subj1.concat("chemistry")+"<br>");
+
+document.write(subj1.concat(subj2))
