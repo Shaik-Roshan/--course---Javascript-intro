bd0740133ed26bd192e0ff12b24247c470eef102
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-26 23:56:37 +0530

    Array_splice

---
+++ js/Array_splice.js
@@ -0,0 +1,7 @@
+// Array splice syntax
+//splice(index, howmany delete(additional)(should give '0'), "newvalue")
+
+var a=["raju", "ravi","rehman","karan"];
+document.write(a+"<br>");
+a.splice(2,0,"roshan","zameer");
+document.write(a);
