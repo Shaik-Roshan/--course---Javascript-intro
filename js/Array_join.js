39018d04b562a42501182b97c9f11eb911269b8e
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-26 23:36:10 +0530

    Array_join

--- js/Array_join.js
+++ js/Array_join.js
@@ -1,1 +1,9 @@
 //Array join
+
+var movies = ["candy", "villan", "hero_NO.1", "trill"];
+var Teater = ["vasavi", "slv", "kps"];
+var Show = movies.concat(Teater);
+document.write(Show+"<br>");
+
+var all = Show.join(" ");
+document.write(all);
