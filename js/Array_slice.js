8437442004bedde184be5b3f7b5e97123625ad82
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-26 23:45:38 +0530

    Array_slice

--- js/Array_slice.js
+++ js/Array_slice.js
@@ -1,1 +1,6 @@
 //Array slice
+
+var college=["oxfor","sastra","dayanand sagar","new horizon"];
+document.write(college +"<br><br>");
+var alter = college.slice(1,3)//alice(start,end) can alo take negative indexing value
+document.write(alter);
