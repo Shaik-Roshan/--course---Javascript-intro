bff9eb1ca5e93a17aa65176e0b54eebcc2b8560e
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-27 23:56:53 +0530

    last_index_of

---
+++ js/last_index_of.js
@@ -0,0 +1,12 @@
+//last_index_of
+
+//it will start searching value from last
+//syntax
+// indexOf("search item",Start)
+
+var a=["abdul","ravi","sidharat","abhi","ravi"]
+document.write(a+"<br>");
+var b=a.lastIndexOf("ravi");
+document.write(b)
+
+//by default if value dont match it return -1 value
