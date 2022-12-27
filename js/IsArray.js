8272f99dfab0e2dcd7911109fd91d5d4e80e2eb3
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-27 23:34:22 +0530

    is_Array

---
+++ js/IsArray.js
@@ -0,0 +1,20 @@
+var a=["ajay","aqib","sanjay","rehman"];
+var b= "roshan";
+var c=22;
+
+document.write(a+"<br>")
+document.write(b+"<br>")
+document.write(c+"<br>")
+
+//to check whether it is array or not for Array
+var d=Array.isArray(a);
+document.write(d+"<br>")
+
+//to check whether it is array or not for String
+var e=Array.isArray(b);
+document.write(e+"<br>")
+
+//to check whether it is array or not for integer
+var f=Array.isArray(c);
+document.write(f+"<br>")
+
