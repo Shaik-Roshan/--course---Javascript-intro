418d44894b523fc4d71ca644e7353836cce4f15e
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-30 23:38:09 +0530

    Array_filter

--- js/Array_filter.js
+++ js/Array_filter.js
@@ -1,1 +1,7 @@
 //Array_filter
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+const result = words.filter(word => word.length > 6);
+
+document.write(result);
+// expected output: Array ["exuberant", "destruction", "present"]
