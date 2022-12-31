8385c7e89f6cd01bedc0333f716f0b00b0affb8d
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-31 22:48:30 +0530

    Array_toString

--- js/Array_toString.js
+++ js/Array_toString.js
@@ -19,3 +19,9 @@ document.write(typeof(b)+"<br>");//string
 
 
 
+//Array -valueOf() Function:
+var friends =["roshan","zameer","shaik"];
+document.write(friends+"<br>");//roshan,zameer,shaik
+friends.valueOf();
+
+document.write(friends+"<br>");//roshan,zameer,shaik it return just same as document.write()
