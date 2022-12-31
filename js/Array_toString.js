f119f53668748e5e4e27ab941659fb29d961d84c
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-31 22:51:20 +0530

    Array_toString

--- js/Array_toString.js
+++ js/Array_toString.js
@@ -25,3 +25,11 @@ document.write(friends+"<br>");//roshan,zameer,shaik
 friends.valueOf();
 
 document.write(friends+"<br>");//roshan,zameer,shaik it return just same as document.write()
+
+
+//Array -fill() Function:
+var subjects=["English","maths","science"];
+document.write(subjects+"<br>");
+
+subjects.fill("commerce");
+document.write(subjects+"<br>");
