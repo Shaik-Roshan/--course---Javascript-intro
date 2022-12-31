f3ff2872308835e75627e9867724449c5f6d54f6
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2022-12-31 22:42:51 +0530

    Array_toString

--- js/Array_toString.js
+++ js/Array_toString.js
@@ -2,5 +2,20 @@
 
 const array1 = [1, 2, 'a', '1a'];
 
-document.write(array1.toString());
+document.write(array1.toString()+"<br>");
 // expected output: "1,2,a,1a"
+
+//Array -toString() Function:
+
+var a=["sanjay","Aman","Rehman"];
+
+document.write(a+"<br>");
+var b=a.toString();
+
+document.write(b+"<br>");
+
+document.write(typeof(a)+"<br>");//object
+document.write(typeof(b)+"<br>");//string
+
+
+
