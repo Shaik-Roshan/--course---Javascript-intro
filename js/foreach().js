13630101607f5a7179d056bdadb030aee2f02b44
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-01 23:57:39 +0530

    forEach Added

--- js/foreach().js
+++ js/foreach().js
@@ -1,7 +1,14 @@
 
-//forEach() Function:
+// //forEach() Function:
 var a=["Rahul","Karan","Aman","Neha"];
-document.write(a+"<br>");
-a.forEach(function(value,index){
-    document.write(index + " "+value+"<br>")
-})
+// document.write(a+"<br>");
+// a.forEach(function(value,index){
+//     document.write(index + " "+value+"<br>")
+// })
+
+// another method
+a.forEach(loop);
+
+function loop( value ,index){
+    document.write(index+" "+ value+"<br>");
+}
