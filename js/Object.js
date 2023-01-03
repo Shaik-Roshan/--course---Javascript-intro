b9d8c2730cf5193516539383e4134d935360367c
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-03 23:17:27 +0530

    Object

--- js/Object.js
+++ js/Object.js
@@ -1,1 +1,23 @@
 //object 
+var a={
+    fname:"roshan",
+    lname:"zameer",
+    age:21,
+    country:"india",
+    gmail:"roshan@gmail.com",
+    cars:["CADILLAC","TATA","BMW","AUDI"],
+    salary:function(){
+        return 25000;
+    },
+    fullName:function(){
+        return fname+" "+lname;
+    }
+
+}
+document.write(a+"<br>");
+document.write(a.fname+"<br>");
+document.write(a.cars+"<br>");
+document.write(a.cars[2]+"<br>");
+document.write(a.salary()+"<br>");
+document.write(a.fullName());
+
