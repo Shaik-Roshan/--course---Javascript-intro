cfbadf004828daebac54e7c15aeeeaf748b3369e
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-04 18:47:57 +0530

    Object_2 Added

---
+++ js/Object_2.js
@@ -0,0 +1,13 @@
+var companies=new Object();// here we are just defining companies later we can put value in that
+
+companies.name1="TATA CONSULTANCY",
+companies.name2="brillio",
+companies.name3="Microsoft",
+companies.name4="Cognizant";
+
+document.write(companies+"<br>");// if we use console we can see key and value of object in console
+document.write(companies.name1+"<br>");
+document.write(companies['name4']);//another method to print name2
+
+
+
