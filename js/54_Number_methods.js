be0c9376fd48d2f50eda9f37f83e4ff2be92dd32
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-07 23:51:25 +0530

    Number_method Added

---
+++ js/54_Number_methods.js
@@ -0,0 +1,16 @@
+// converting other types into number
+
+//number()
+var a="99";
+var b=Number(a);
+document.write(typeof(a)+"<br>");//string
+document.write(typeof(b)+"<br>");//number
+
+document.write(b+1+"<br>");//100
+
+// parseInt()
+var num1="121 65 95";
+var result1=parseInt(num1+"<br>");
+var num2=101.25;
+var result2=parseInt(num2)
+document.write(result1)
