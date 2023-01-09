2379f0f7f427a3e5c431158eb7bb8348634c3861
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-09 23:57:08 +0530

    date Methods

--- js/56_Date_Method.js
+++ js/56_Date_Method.js
@@ -14,3 +14,15 @@
 // 14.setMinutes()
 // 15.setMonth()
 // 16.setSeconds()
+
+var date=new Date()
+document.write(date+"<br>");//Mon Jan 09 2023 23:40:53 GMT+0530 (India Standard Time)
+document.write(date.toDateString()+"<br>");//Mon Jan 09 2023
+document.write(date.getDate()+"<br>");//9
+document.write(date.getFullYear()+"<br>");//2023
+document.write(date.getMonth()+"<br>");
+document.write(date.getDay()+"<br>");
+document.write(date.getHours()+"<br>");
+document.write(date.getMinutes()+"<br>");
+document.write(date.getSeconds()+"<br>");
+document.write(date.getMilliseconds()+"<br>");
