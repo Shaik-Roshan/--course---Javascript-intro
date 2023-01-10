edc01541958bfb73ca1b3b04126d4fe89d74748a
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-10 20:09:52 +0530

    date Methods

---
+++ js/57_setDate().js
@@ -0,0 +1,20 @@
+var now=new Date();
+document.write(now+"<br>");
+now.setDate(11);
+document.write(now+"<br>");
+
+now.setFullYear(2001);
+document.write(now+"<br>");
+
+now.setMonth(2);//jan-->0,ferb-->1,march-->3...
+document.write(now+"<br>");
+
+now.setDate(30);
+document.write(now+"<br>");
+
+
+
+// now.setDay(fri);
+// document.write(now+"<br>");
+
+
