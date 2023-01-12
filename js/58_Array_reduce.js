885824d2d6bfb09ea668c3b901f09f731b542618
shaikroshanzameer8 <shaikroshanzameer8@gmail.com>
2023-01-12 23:27:55 +0530

    Added

---
+++ js/58_Array_reduce.js
@@ -0,0 +1,27 @@
+const arr = [2, 3, 4, 5, 6];
+
+
+// document.write(arr.reduce((a, b) => a + b) + "<br>");
+// // document.write(arr);
+
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// document.write(addition(1, 2));
+// var a = 10;
+// var b = 20;
+var c = (a, b) => {
+  a + b;
+};
+document.write("<br>" + c);
+
+
+function a(){};
+
+a(); // CALLING
+
+const  Func = function b(){};
+
+(function c(){}) ()//iife
+
